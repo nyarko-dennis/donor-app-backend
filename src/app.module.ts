82f54186a1cf733fddb7a2d9bc7a1b56b0450088
Dennis Nyarko-Antwi <ndenniskofi@gmail.com>
2026-02-15 22:07:07 +0000

    feat: Introduce initial data migrations for constituencies and donation causes, and define role-based access control.

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,16 +21,22 @@ import { QueueModule } from './queue/queue.module';
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: true, // Auto-create tables (dev only)
-      }),
+      useFactory: (configService: ConfigService) => {
+        //const isProduction = configService.get('NODE_ENV') === 'production';
+        //const dbSsl = configService.get('DB_SSL') === 'true';
+
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: true, // Auto-create tables (dev only)
+          ssl: { rejectUnauthorized: false },
+        };
+      },
       inject: [ConfigService],
     }),
     CampaignsModule,
