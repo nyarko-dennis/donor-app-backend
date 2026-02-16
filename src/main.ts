246a247772d483894639df4b6cef80310bea81a7
Dennis Nyarko-Antwi <ndenniskofi@gmail.com>
2026-02-16 06:42:19 +0000

    feat: Add RBAC documentation and implement initial data migration for constituencies and donation causes with generation scripts.

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   });
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'http://localhost:3005', 'https://bunny-suitable-conversely.ngrok-free.app', 'https://gisebidonorplatformcms-555954816336.us-central1.run.app', 'https://gisebidonorplatformpay-555954816336.us-central1.run.app'],
+    origin: ['http://localhost:3001', 'http://localhost:3005', 'https://bunny-suitable-conversely.ngrok-free.app', 'https://pay.gisdonorman.goyazi.com', 'https://gisdonorman.goyazi.com'],
     credentials: true,
   });
   app.setGlobalPrefix('api');
